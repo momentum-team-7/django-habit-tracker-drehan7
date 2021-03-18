5fc4d5a1a760583dcbd4faae058cad41dc01ef84
Dawud Rehan <d.rehan7@gmail.com>
2021-03-18 18:30:51 -0400

    tryna fix

--- core/models.py
+++ core/models.py
@@ -5,7 +5,29 @@ import psycopg2
 class User(AbstractUser):
     pass
 
-class Habit(model.Models):
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
 
-    habit = models.CharField(max_length=150)
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class HabitLog(models.Model):
+    date = models.DateField(auto_now_add=True)
+    track_unit = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return f"Date: {self.date} | Amount: {self.track_unit}"
+
+class Habit(model.Models):
+    title = models.CharField(max_length = 100)
+    description = models.CharField(max_length=200)
+    log = models.ForeignKey(HabitLog, on_delete=models.CASCADE, blank=True, null=True)
+    goal = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"Title: {self.title} | desc: {self.description} | Goal: {self.goal}"
+
     
